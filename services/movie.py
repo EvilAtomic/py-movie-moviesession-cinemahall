2f60bcb1f5bf86cab97150c75fac84e86934eb4d
artem <artemasarkisan598@gmail.com>
2026-03-06 17:11:43 +0200

    py-movie-moviesession-cinemahall

--- services/movie.py
+++ services/movie.py
@@ -6,7 +6,7 @@ from db.models import Movie
 def get_movies(
     genres_ids: Optional[List[int]] = None,
     actors_ids: Optional[List[int]] = None,
-) -> QuerySet:
+) -> QuerySet[Movie]:
     queryset = Movie.objects.all()
 
     if genres_ids and actors_ids:
@@ -15,9 +15,11 @@ def get_movies(
             actors__id__in=actors_ids,
         )
     elif genres_ids:
-        queryset = queryset.filter(genres__id__in=genres_ids)
+        queryset =\
+            queryset.filter(genres__id__in=genres_ids)
     elif actors_ids:
-        queryset = queryset.filter(actors__id__in=actors_ids)
+        queryset =\
+            queryset.filter(actors__id__in=actors_ids)
 
     return queryset.distinct()
 
