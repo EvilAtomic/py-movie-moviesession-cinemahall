b7fcceefacf249ccaf9c0dbd170b04958a5a8fff
artem <artemasarkisan598@gmail.com>
2026-03-06 16:48:48 +0200

    py-movie-moviesession-cinemahall

--- services/movie.py
+++ services/movie.py
@@ -10,7 +10,7 @@ def get_movies(genres_ids=None, actors_ids=None) -> QuerySet:
         queryset = queryset.filter(genres__id__in=genres_ids)
     elif actors_ids:
         queryset = queryset.filter(actors__id__in=actors_ids)
-    return queryset
+    return queryset.distinct()
 
 def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
